e4b78f2b54a434389838bb804955f07c578aff22
Nandeep Shah <nandeep.shah@gmail.com>
2022-10-22 21:23:36 -0400

    Lesson 2

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-const { restart } = require('nodemon');
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use(logger);
+app.use(cors(corsOptions));
+//lets server parse json
+app.use(express.json());
+app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/root'));
+
+//sends a 404 after all middleware didnt catch the request
 app.all('*', (req, res) => {
 	res.status(404);
 	if (req.accepts('html')) {
@@ -16,4 +27,6 @@ app.all('*', (req, res) => {
 		res.type('txt').send('404 not found');
 	}
 });
+
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
